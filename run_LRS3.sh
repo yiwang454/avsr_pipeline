a3f7b324b0ea00d01d7abc96acf0107e9beb9910
ylrsh wa <yiwang454@gmail.com>
2024-03-11 21:38:05 +0000

    change running script2

--- run_LRS3.sh
+++ run_LRS3.sh
@@ -1,5 +1,6 @@
   resampled_dir="muavic/it/video/train"
   output_file="/mnt/ceph_rbd/muavic_project/avsr_pipeline/segments.csv"
+  cd /mnt/ceph_rbd/muavic_project
   for i in 1; do
     python3 avsr_pipeline/run.py --device cuda:$((i-1)) $resampled_dir $output_file
   done
