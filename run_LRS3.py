aa899511a05149b5ce6cd54341fad23c30a60e10
ylrsh wa <yiwang454@gmail.com>
2024-03-13 21:35:19 +0000

    change hyperparam

--- run_LRS3.py
+++ run_LRS3.py
@@ -6,9 +6,9 @@ import os
 GPU_idx = 0
 
 resampled_dir = "/mnt/ceph_rbd/muavic_project/muavic/fr/video/valid_fake"
-output_file = "/mnt/ceph_rbd/muavic_project/avsr_pipeline/segments2.csv"
+output_file = "/mnt/ceph_rbd/muavic_project/avsr_pipeline/segments4.csv"
 root_dir = "/mnt/ceph_rbd/muavic_project/avsr_pipeline"
-command = '''python3 run_multiprocess.py --device cuda:{} --syncnet-threshold 0.5 \
+command = '''python3 run_multiprocess.py --device cuda:{} --syncnet-threshold 5.0 \
             --num-workers 3 {} {}'''.format(GPU_idx, resampled_dir, output_file)
 subp = subprocess.Popen(command, shell=True, cwd=root_dir,
                         encoding="utf-8")  # stdout=subprocess.PIPE, stderr=subprocess.PIPE,
