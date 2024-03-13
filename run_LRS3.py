691d46cdb5ff0ceb5275409a35c0c94de6767b8e
ylrsh wa <yiwang454@gmail.com>
2024-03-13 18:09:36 +0000

    try multiprocessing

--- run_LRS3.py
+++ run_LRS3.py
@@ -5,10 +5,10 @@ import os
 # ss_num = len(GPU_nums)
 GPU_idx = 0
 
-resampled_dir = "/mnt/ceph_rbd/muavic_project/muavic/fr/video/train"
-output_file = "/mnt/ceph_rbd/muavic_project/avsr_pipeline/segments.csv"
+resampled_dir = "/mnt/ceph_rbd/muavic_project/muavic/fr/video/valid_fake"
+output_file = "/mnt/ceph_rbd/muavic_project/avsr_pipeline/segments2.csv"
 root_dir = "/mnt/ceph_rbd/muavic_project/avsr_pipeline"
-command = "python3 run_multiprocess.py --device cuda:{} {} {}".format(GPU_idx, resampled_dir, output_file)
+command = "python3 run_multiprocess.py --device cuda:{} --num-workers 3 {} {}".format(GPU_idx, resampled_dir, output_file)
 subp = subprocess.Popen(command, shell=True, cwd=root_dir,
                         encoding="utf-8")  # stdout=subprocess.PIPE, stderr=subprocess.PIPE,
 subp.wait()
